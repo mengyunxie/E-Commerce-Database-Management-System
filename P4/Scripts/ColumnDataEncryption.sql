a17fcb2aa078d1226cd12723c4ca03ee531c8683
Nia Chenni Xu <chennixu1998@gmail.com>
2025-04-03 14:27:41 -0700

    Added Encryption

---
+++ P4/Scripts/ColumnDataEncryption.sql
@@ -0,0 +1,16 @@
+USE E_Commerce;
+
+-- Create Master Key
+CREATE MASTER KEY
+ENCRYPTION BY PASSWORD = 'e_commerce';
+
+-- Create Certificate
+CREATE CERTIFICATE TestCertificate
+WITH SUBJECT = 'Protect e_commerce data';
+
+-- Create Symmetric Key
+CREATE SYMMETRIC KEY TestSymmetricKey
+WITH ALGORITHM = AES_256
+ENCRYPTION BY CERTIFICATE TestCertificate;
+
+
